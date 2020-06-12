01567246b7007ab21fdbdc045fa08af01ee15d9e
Jefferson M Souza <jefferson.msouza86@gmail.com>
2020-06-11 23:04:54 -0300

    update sw path

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // if ('serviceWorker' in navigator) {
 //   window.addEventListener('load', () => {
 //     navigator.serviceWorker
-//       .register('/sw.js')
+//       .register('sw.js')
 //       .then((registration) => {
 //         console.log('SW registered: ', registration);
 //       })
